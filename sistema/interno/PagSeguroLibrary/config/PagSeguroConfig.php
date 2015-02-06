d10e8fca861e9599880c0da66887ee60d26702c4
jonathan <jonanbh@gmail.com>
2015-02-06 16:48:04 -0200

    reparo no redirecionamento de post

--- sistema/interno/PagSeguroLibrary/config/PagSeguroConfig.php
+++ sistema/interno/PagSeguroLibrary/config/PagSeguroConfig.php
@@ -8,12 +8,12 @@
 
 $PagSeguroConfig = array();
 
-$PagSeguroConfig['environment'] = "production"; // production, sandbox
+$PagSeguroConfig['environment'] = "sandbox"; // production, sandbox
 
 $PagSeguroConfig['credentials'] = array();
 $PagSeguroConfig['credentials']['email'] = "duvidahomeopatia@terra.com.br";
 $PagSeguroConfig['credentials']['token']['production'] = "DE6677BCAA4944A78CAD60393920C44F";
-$PagSeguroConfig['credentials']['token']['sandbox'] = "";
+$PagSeguroConfig['credentials']['token']['sandbox'] = "0061B39414444850AB060977BA356EE0";
 
 $PagSeguroConfig['application'] = array();
 $PagSeguroConfig['application']['charset'] = "UTF-8"; // UTF-8, ISO-8859-1
